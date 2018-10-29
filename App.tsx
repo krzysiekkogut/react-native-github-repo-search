6e0104bb46162b25bec28c880f616de3c3d3cfaa
Krzysztof Kogut <krzysztof.kogut@altimi.com>
2018-10-29 13:42:55 +0100

    Add basic structure

--- App.tsx
+++ App.tsx
@@ -1,22 +1,28 @@
 import React from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import axiosMiddleware from 'redux-axios-middleware';
+import axios from 'axios';
+
+import { reducer } from './src/reducer';
+import GithubRepos from './src/GithubRepos';
+
+const client = axios.create({
+  baseURL: 'https://api.github.com',
+  responseType: 'json',
 });
+const store = createStore(reducer, applyMiddleware(axiosMiddleware(client)));
 
-type Props = {};
-export default class App extends React.Component<Props> {
+export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.tsx</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
+      <Provider store={store}>
+        <View style={styles.container}>
+          <GithubRepos />
+        </View>
+      </Provider>
     );
   }
 }
@@ -24,18 +30,7 @@ export default class App extends React.Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+    backgroundColor: '#fff',
+    paddingTop: 50,
   },
 });
---
+++ src/reducer.ts
@@ -0,0 +1,10 @@
+import { Action } from 'redux'
+
+const initialState = {};
+
+export function reducer(state = initialState, action: Action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
