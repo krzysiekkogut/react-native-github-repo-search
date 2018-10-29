c23dee69b9a321e8ac3e3b00f03530111d1988a7
Krzysztof Kogut <krzysiek.kogut@outlook.com>
2018-10-29 19:21:33 +0100

    Add ActivityIndicator to Search component

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
-import { TextInput, StyleSheet } from 'react-native';
+import { View, TextInput, ActivityIndicator, StyleSheet } from 'react-native';
 import { connect } from 'react-redux';
 import { ReposState } from '../redux/reducer';
 
 import { fetchRepos, clearRepos, showValidationError } from '../redux/reducer';
 
-interface Props {
+interface Actions {
   fetchRepos: Function;
   clearRepos: Function;
   showValidationError: Function;
 }
 
-export class Search extends React.Component<Props> {
+interface Props {
+  loading: boolean;
+}
+
+export class Search extends React.Component<Props & Actions> {
   private timeoutId?: number;
 
   private onChangeText = (searchText: string) => {
@@ -21,36 +25,50 @@ export class Search extends React.Component<Props> {
         this.props.clearRepos();
         return;
       }
-  
+
       if (searchText.toLocaleLowerCase() === searchText) {
         this.props.fetchRepos(searchText);
         return;
       }
-  
+
       this.props.showValidationError(searchText);
     }, 500);
   }
 
   render() {
-
     return (
-      <TextInput
-        style={styles.searchInput}
-        placeholder='Repository name'
-        onChangeText={this.onChangeText}
-      />
+      <View style={styles.container}>
+        <TextInput
+          style={styles.searchInput}
+          placeholder='Repository name'
+          onChangeText={this.onChangeText}
+        />
+        {this.props.loading && <ActivityIndicator size='small' style={styles.spinner} />}
+      </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  searchInput: {
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    width: '100%',
     borderBottomWidth: 1,
     borderBottomColor: '#000',
-    width: '100%',
+  },
+  searchInput: {
+    flex: 0.8,
+  },
+  spinner: {
+    flex: 0.2,
   },
 });
 
+const mapStateToProps = (state: ReposState): Props => {
+  return { loading: state.loading };
+};
+
 const mapDispatchToProps = { fetchRepos, clearRepos, showValidationError };
 
-export default connect(null, mapDispatchToProps)(Search);
+export default connect(mapStateToProps, mapDispatchToProps)(Search);
